c96e09c289a5c95f0adcff1eeac6b0dbdf4f639f
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-02 17:24:27 +0100

    Exercises 4-1 and 4-2 (for loops)

---
+++ chapter_4.py
@@ -0,0 +1,14 @@
+# Exercise 4-1:
+
+pizzas = ["margherita", "calabrese", "fiorentina"]
+for pizza in pizzas:
+    print(f"One of my favourite pizzas is the {pizza.title()}.")
+print("I am trying to eat fewer pizzas.\n")
+
+
+# Exercise 4-2:
+
+animals = ["cat", "dog", "squirrel"]
+for animal in animals:
+    print(f"A {animal} has a tail.")
+print("All of the animals above have tails.\n")
