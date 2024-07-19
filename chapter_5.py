32c0709e58debb4331608991b204cc80784093c1
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-19 17:24:53 +0100

    Exercise 5-1, conditional tests

---
+++ chapter_5.py
@@ -0,0 +1,39 @@
+#  Exercise 5-1
+
+dinner = "pizza"
+print("Is dinner == 'pizza'? I predict True.")
+print(dinner == "pizza")
+
+print("\nIs dinner == 'salad'? I predict False.")
+print(dinner == "salad")
+
+print(
+    "\nIs dinner == 'Pizza', given that testing for equality is case sensitive in Python? I predict False."
+)
+print(dinner == "Pizza")
+
+dessert = "bread and butter pudding"
+print("\nIs dessert == 'bread and butter pudding'? I predict True.")
+print(dessert == "bread and butter pudding")
+
+print("\nIs dessert == 'ice cream'? I predict False.")
+print(dessert == "ice cream")
+
+tv = "Million Dollar Listing"
+print("\nIs tv == 'Million Dollar Listing'? I predict True.")
+print(tv == "Million Dollar Listing")
+
+magazine = "Grazia"
+print("\nIs magazine == 'Grazia'? I predict True.")
+print(magazine == "Grazia")
+
+print("\nIs magazine == 'grazia'? I predict False.")
+print(magazine == "grazia")
+
+neighbourhood = "Harborne"
+print("\nIs neighbourhood != 'Harborne'? I predict False.")
+print(neighbourhood != "Harborne")
+
+number = 1000
+print("\nIs number < 2000? I predict True.")
+print(number < 2000)
