f1c50f9821f7d81e70f2c694e29b0c0ccbe986dc
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-22 13:51:15 +0100

    Exercise 5-4, if-else

--- chapter_5/if_statements.py
+++ chapter_5/if_statements.py
@@ -5,7 +5,24 @@
 # if alien_color == "green":
 #     print("You have earned 5 points!")
 
-alien_color = "red"
+# alien_color = "red"
 
-if alien_color == "green":
+# if alien_color == "green":
+#     print("You have earned 5 points!")
+
+
+# Exercise 5-4
+
+alien_colour = "green"
+
+if alien_colour == "green":
     print("You have earned 5 points!")
+else:
+    print("You have earned 10 points!")
+
+alien_colour = "yellow"
+
+if alien_colour == "green":
+    print("You have earned 5 points!")
+else:
+    print("You have earned 10 points!")
