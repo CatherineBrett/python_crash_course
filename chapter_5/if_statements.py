b17276248046a7638062baa14799a974838c898c
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-22 13:58:02 +0100

    Exercise 5-5, if-elif-else chains

--- chapter_5/if_statements.py
+++ chapter_5/if_statements.py
@@ -13,16 +13,46 @@
 
 # Exercise 5-4
 
-alien_colour = "green"
+# alien_colour = "green"
 
-if alien_colour == "green":
+# if alien_colour == "green":
+#     print("You have earned 5 points!")
+# else:
+#     print("You have earned 10 points!")
+
+# alien_colour = "yellow"
+
+# if alien_colour == "green":
+#     print("You have earned 5 points!")
+# else:
+#     print("You have earned 10 points!")
+
+
+# Exercise 5-5
+
+alien_color = "green"
+
+if alien_color == "green":
     print("You have earned 5 points!")
+elif alien_color == "yellow":
+    print("You have earned 10 points")
 else:
-    print("You have earned 10 points!")
+    print("You have earned 15 points!")
 
-alien_colour = "yellow"
+alien_color = "yellow"
 
-if alien_colour == "green":
+if alien_color == "green":
     print("You have earned 5 points!")
+elif alien_color == "yellow":
+    print("You have earned 10 points")
 else:
-    print("You have earned 10 points!")
+    print("You have earned 15 points!")
+
+alien_color = "red"
+
+if alien_color == "green":
+    print("You have earned 5 points!")
+elif alien_color == "yellow":
+    print("You have earned 10 points")
+else:
+    print("You have earned 15 points!")
