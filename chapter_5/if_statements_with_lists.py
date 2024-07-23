efffe59bb1f3d3b70b87c89852e7494694b9ec31
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-23 13:21:30 +0100

    Exercise 5-9, if statements with lists

--- chapter_5/if_statements_with_lists.py
+++ chapter_5/if_statements_with_lists.py
@@ -1,9 +1,23 @@
 # Exercise 5-8
 
-usernames = ["catherineb", "michaelb", "elizabeths", "alaindb", "admin"]
+# usernames = ["catherineb", "michaelb", "elizabeths", "alaindb", "admin"]
 
-for username in usernames:
-    if username == "admin":
-        print("Hi admin! Would you like to see a status report?")
-    else:
-        print(f"Hello {username}. Welcome back!")
+# for username in usernames:
+#     if username == "admin":
+#         print("Hi admin! Would you like to see a status report?")
+#     else:
+#         print(f"Hello {username}. Welcome back!")
+
+
+# Exercise 5-9
+
+usernames = []
+
+if usernames:
+    for username in usernames:
+        if username == "admin":
+            print("Hi admin! Would you like to see a status report?")
+        else:
+            print(f"Hello {username}. Welcome back!")
+else:
+    print("We need to find some users!")
