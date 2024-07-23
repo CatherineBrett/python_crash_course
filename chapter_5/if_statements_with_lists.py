0ce1d8b8cbd7c2892af70c96774c2c03f42cecc7
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-23 17:23:00 +0100

    Exercise 5-11, if statements with lists

--- chapter_5/if_statements_with_lists.py
+++ chapter_5/if_statements_with_lists.py
@@ -25,21 +25,36 @@
 
 # Exercise 5-10
 
-current_users = ["caln", "Georges", "craigb", "michelb", "WendyP", "John"]
-new_users = ["georges", "gorev", "bertrandr", "wendyp", "markh", "JOHN"]
+# current_users = ["caln", "Georges", "craigb", "michelb", "WendyP", "John"]
+# new_users = ["georges", "gorev", "bertrandr", "wendyp", "markh", "JOHN"]
 
-current_users_copy = []
-for current_user in current_users:
-    current_user_lower = current_user.lower()
-    current_users_copy.append(current_user_lower)
+# current_users_copy = []
+# for current_user in current_users:
+#     current_user_lower = current_user.lower()
+#     current_users_copy.append(current_user_lower)
 
-new_users_copy = []
-for new_user in new_users:
-    new_user_lower = new_user.lower()
-    new_users_copy.append(new_user_lower)
+# new_users_copy = []
+# for new_user in new_users:
+#     new_user_lower = new_user.lower()
+#     new_users_copy.append(new_user_lower)
 
-for username in new_users_copy:
-    if username in current_users_copy:
-        print("Username not available. Please enter a new username.")
+# for username in new_users_copy:
+#     if username in current_users_copy:
+#         print("Username not available. Please enter a new username.")
+#     else:
+#         print("Username available.")
+
+
+# Exercise 5-11
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for num in nums:
+    if num == 1:
+        print(f"{num}st")
+    elif num == 2:
+        print(f"{num}nd")
+    elif num == 3:
+        print(f"{num}rd")
     else:
-        print("Username available.")
+        print(f"{num}th")
