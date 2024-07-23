bafab33f6b7163b08744cb774387b03e609d969f
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-23 14:26:17 +0100

    Exercise 5-10, if statements with lists

--- chapter_5/if_statements_with_lists.py
+++ chapter_5/if_statements_with_lists.py
@@ -11,13 +11,35 @@
 
 # Exercise 5-9
 
-usernames = []
+# usernames = []
 
-if usernames:
-    for username in usernames:
-        if username == "admin":
-            print("Hi admin! Would you like to see a status report?")
-        else:
-            print(f"Hello {username}. Welcome back!")
-else:
-    print("We need to find some users!")
+# if usernames:
+#     for username in usernames:
+#         if username == "admin":
+#             print("Hi admin! Would you like to see a status report?")
+#         else:
+#             print(f"Hello {username}. Welcome back!")
+# else:
+#     print("We need to find some users!")
+
+
+# Exercise 5-10
+
+current_users = ["caln", "Georges", "craigb", "michelb", "WendyP", "John"]
+new_users = ["georges", "gorev", "bertrandr", "wendyp", "markh", "JOHN"]
+
+current_users_copy = []
+for current_user in current_users:
+    current_user_lower = current_user.lower()
+    current_users_copy.append(current_user_lower)
+
+new_users_copy = []
+for new_user in new_users:
+    new_user_lower = new_user.lower()
+    new_users_copy.append(new_user_lower)
+
+for username in new_users_copy:
+    if username in current_users_copy:
+        print("Username not available. Please enter a new username.")
+    else:
+        print("Username available.")
