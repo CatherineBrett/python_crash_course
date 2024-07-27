99f139a0f03b3118f44c01a0aa955bb3aff02626
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-27 14:31:07 +0100

    Exercise 6-3, working with dictionaries

--- chapter_06_dictionaries/working_with_dictionaries.py
+++ chapter_06_dictionaries/working_with_dictionaries.py
@@ -6,6 +6,7 @@ print(f"Their last name is {an_on["last_name"].title()}.")
 print(f"They are {an_on["age"]} years old.")
 print(f"They live in {an_on["city"].title()}.\n")
 
+
 # Exercise 6-2
 
 fave_numbers = {
@@ -17,3 +18,29 @@ fave_numbers = {
     }
 
 print(fave_numbers)
+
+
+# Exercise 6-3
+
+glossary = {
+    "variable": "a label you can assign to a value",
+    "string": "anything inside quotes",
+    "float": "any number with a decimal point",
+    "list": "a collection of items in a particular order",
+    "tuple": "an immutable list",
+    }
+
+term = glossary["variable"]
+print(f"\nVariable: {term}\n")
+
+term = glossary["string"]
+print(f"String: {term}\n")
+
+term = glossary["float"]
+print(f"Float: {term}\n")
+
+term = glossary["list"]
+print(f"List: {term}\n")
+
+term = glossary["tuple"]
+print(f"Tuple: {term}\n")
