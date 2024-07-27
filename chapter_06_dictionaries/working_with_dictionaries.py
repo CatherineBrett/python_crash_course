1335187801a9366bc2ab10e5380eeac549812536
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-27 16:08:18 +0100

    Exercise 6-3 (edited) and exercise 6-4, looping through a dictionary

--- chapter_06_dictionaries/working_with_dictionaries.py
+++ chapter_06_dictionaries/working_with_dictionaries.py
@@ -20,27 +20,35 @@ fave_numbers = {
 print(fave_numbers)
 
 
-# Exercise 6-3
+# Exercises 6-3 and 6-4
 
 glossary = {
     "variable": "a label you can assign to a value",
     "string": "anything inside quotes",
+    "whitespace": "any nonprinting characters, such as spaces, tabs, and newlines",
     "float": "any number with a decimal point",
+    "index error": "occurs when Python can't find an item at the index you requested",
     "list": "a collection of items in a particular order",
     "tuple": "an immutable list",
+    "boolean expression": "another name for a conditional test",
+    "dictionary": "a collection of key-value pairs",
+    "key-value pair": "a set of values associated with each other",
     }
 
-term = glossary["variable"]
-print(f"\nVariable: {term}\n")
+# definition = glossary["variable"]
+# print(f"\nVariable: {definition}\n")
 
-term = glossary["string"]
-print(f"String: {term}\n")
+# definition = glossary["string"]
+# print(f"String: {definition}\n")
 
-term = glossary["float"]
-print(f"Float: {term}\n")
+# definition = glossary["float"]
+# print(f"Float: {definition}\n")
 
-term = glossary["list"]
-print(f"List: {term}\n")
+# definition = glossary["list"]
+# print(f"List: {definition}\n")
 
-term = glossary["tuple"]
-print(f"Tuple: {term}\n")
+# definition = glossary["tuple"]
+# print(f"Tuple: {definition}\n")
+
+for term, definition in glossary.items():
+    print(f"\n{term.title()}: {definition}")
