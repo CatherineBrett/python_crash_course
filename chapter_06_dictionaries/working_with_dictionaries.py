9a54abbb4ec70072150f8008e3c419f2a5591b8a
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-27 13:40:21 +0100

    Exercise 6-1, working with dictionaries

---
+++ chapter_06_dictionaries/working_with_dictionaries.py
@@ -0,0 +1,7 @@
+# Exercise 6-1
+
+an_on = {"first_name": "an", "last_name": "on", "age": 1000, "city": "naples"}
+print(f"This person's first name is {an_on["first_name"].title()}.")
+print(f"Their last name is {an_on["last_name"].title()}.")
+print(f"They are {an_on["age"]} years old.")
+print(f"They live in {an_on["city"].title()}.")
