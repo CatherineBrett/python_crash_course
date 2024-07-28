c24e6021af6d531775e983944b2deec9c2dc35ea
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-28 18:31:46 +0100

    Exercise 6-8, nesting

--- chapter_06_dictionaries/nesting.py
+++ chapter_06_dictionaries/nesting.py
@@ -1,15 +1,28 @@
 # Exercise 6-7
 
-an_on = {"first_name": "an", "last_name": "on", "age": 1000, "city": "naples"}
-jbloggs = {"first_name": "joe", "last_name": "bloggs", "age": 50, "city": "bonn"}
-cnewport = {
-    "first_name": "cal",
-    "last_name": "newport",
-    "age": 42,
-    "city": "washington, d.c.",
-}
+# an_on = {"first_name": "an", "last_name": "on", "age": 1000, "city": "naples"}
+# jbloggs = {"first_name": "joe", "last_name": "bloggs", "age": 50, "city": "bonn"}
+# cnewport = {
+#     "first_name": "cal",
+#     "last_name": "newport",
+#     "age": 42,
+#     "city": "washington, d.c.",
+# }
 
-people = [an_on, jbloggs, cnewport]
+# people = [an_on, jbloggs, cnewport]
 
-for person in people:
-    print(person)
+# for person in people:
+#     print(person)
+
+
+# Exercise 6-8
+
+bobby = {"animal": "dog", "owner": "sue"}
+india = {"animal": "dog", "owner": "maala"}
+chase = {"animal": "cat", "owner": "jules"}
+freddy = {"animal": "fox", "owner": "colette"}
+
+pets = [bobby, india, chase, freddy]
+
+for pet in pets:
+    print(pet)
