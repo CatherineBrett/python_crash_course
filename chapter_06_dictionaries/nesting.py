ba861da211eee520fe496a680fb6c0ad39225b94
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-28 18:20:46 +0100

    Exercise 6-7, nesting

---
+++ chapter_06_dictionaries/nesting.py
@@ -0,0 +1,15 @@
+# Exercise 6-7
+
+an_on = {"first_name": "an", "last_name": "on", "age": 1000, "city": "naples"}
+jbloggs = {"first_name": "joe", "last_name": "bloggs", "age": 50, "city": "bonn"}
+cnewport = {
+    "first_name": "cal",
+    "last_name": "newport",
+    "age": 42,
+    "city": "washington, d.c.",
+}
+
+people = [an_on, jbloggs, cnewport]
+
+for person in people:
+    print(person)
