9bf8217da8d0d7a3485bce2f04d48a8d60dd673b
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-28 19:19:04 +0100

    Exercise 6-11, nesting

--- chapter_06_dictionaries/nesting.py
+++ chapter_06_dictionaries/nesting.py
@@ -47,18 +47,51 @@
 
 # Exercise 6-10
 
-fave_numbers = {
-    "john": [1, 6],
-    "paul": [3],
-    "george": [9, 12, 20],
-    "ringo": [0, 300],
-    "pete": [7],
+# fave_numbers = {
+#     "john": [1, 6],
+#     "paul": [3],
+#     "george": [9, 12, 20],
+#     "ringo": [0, 300],
+#     "pete": [7],
+# }
+
+# for name, numbers in fave_numbers.items():
+#     if len(numbers) == 1:
+#         print(f"\n{name.title()}'s favorite number is:")
+#     else:
+#         print(f"\n{name.title()}'s favorite numbers are:")
+#     for number in numbers:
+#         print(f"\t{number}")
+
+
+# Exercise 6-11
+
+cities = {
+    "birmingham": {
+        "country": "england",
+        "population": 1157603,
+        "fact": "Arthur Conan Doyle worked in the Aston area of Birmingham",
+    },
+    "liverpool": {
+        "country": "england",
+        "population": 496770,
+        "fact": "Musicians from the city have produced 58 No. 1 singles, "
+        "more than any other city in the world",
+    },
+    "manchester": {
+        "country": "england",
+        "population": 568996,
+        "fact": "The Guardian newspaper was founded in the city in 1821 "
+        "as The Manchester Guardian",
+    },
 }
 
-for name, numbers in fave_numbers.items():
-    if len(numbers) == 1:
-        print(f"\n{name.title()}'s favorite number is:")
-    else:
-        print(f"\n{name.title()}'s favorite numbers are:")
-    for number in numbers:
-        print(f"\t{number}")
+for city, city_info in cities.items():
+    print(f"\nCity: {city.title()}")
+    country = city_info["country"]
+    population = city_info["population"]
+    fact = city_info["fact"]
+
+    print(f"\tCountry: {country.title()}")
+    print(f"\tPopulation: {population}")
+    print(f"\tFact: {fact}")
