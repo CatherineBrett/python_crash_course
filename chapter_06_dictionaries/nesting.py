a9de50e7e4c29bc00a61212b137ecc26a91e9bae
Catherine Brett <catherinejanebrett@icloud.com>
2024-07-28 18:45:24 +0100

    Exercise 6-9, nesting

--- chapter_06_dictionaries/nesting.py
+++ chapter_06_dictionaries/nesting.py
@@ -7,7 +7,7 @@
 #     "last_name": "newport",
 #     "age": 42,
 #     "city": "washington, d.c.",
-# }
+#     }
 
 # people = [an_on, jbloggs, cnewport]
 
@@ -17,12 +17,29 @@
 
 # Exercise 6-8
 
-bobby = {"animal": "dog", "owner": "sue"}
-india = {"animal": "dog", "owner": "maala"}
-chase = {"animal": "cat", "owner": "jules"}
-freddy = {"animal": "fox", "owner": "colette"}
+# bobby = {"animal": "dog", "owner": "sue"}
+# india = {"animal": "dog", "owner": "maala"}
+# chase = {"animal": "cat", "owner": "jules"}
+# freddy = {"animal": "fox", "owner": "colette"}
 
-pets = [bobby, india, chase, freddy]
+# pets = [bobby, india, chase, freddy]
 
-for pet in pets:
-    print(pet)
+# for pet in pets:
+#     print(pet)
+
+
+# Exercise 6-9
+
+favorite_places = {
+    "jim": ["paris", "lille"],
+    "frank": ["berlin"],
+    "deirdre": ["seville", "lisbon", "madeira"],
+}
+
+for name, places in favorite_places.items():
+    if len(places) < 2:
+        print(f"\n{name.title()}'s favorite place is:")
+    else:
+        print(f"\n{name.title()}'s favorite places are:")
+    for place in places:
+        print(f"\t{place.title()}")
