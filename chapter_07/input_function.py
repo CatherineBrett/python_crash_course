fc480ba20c08a0677a1505996bd9b88ffed45c39
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-03 12:41:47 +0100

    Exercise 7-3, input function

--- chapter_07/input_function.py
+++ chapter_07/input_function.py
@@ -6,10 +6,21 @@
 
 # Exercise 7-2
 
-group_size = input("How many people are in your party? ")
-group_size = int(group_size)
+# group_size = input("How many people are in your party? ")
+# group_size = int(group_size)
 
-if group_size > 8:
-    print("We will let you know as soon as a table is available.")
+# if group_size > 8:
+#     print("We will let you know as soon as a table is available.")
+# else:
+#     print("Thank you. Your table is ready.")
+
+
+# Exercise 7-3
+
+number = input("Choose a number and I'll tell you if it's a multiple of 10: ")
+number = int(number)
+
+if number % 10 == 0:
+    print(f"The number {number} is indeed a multiple of 10.")
 else:
-    print("Thank you. Your table is ready.")
+    print(f"The number {number} is not a multiple of 10.")
