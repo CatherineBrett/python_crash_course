3e5f7f3d32766cace4a0a3b62e5eb132f51c17f8
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-06 15:11:13 +0100

    Exercise 7-6 pt I, while loops

--- chapter_07/while_loops.py
+++ chapter_07/while_loops.py
@@ -29,17 +29,31 @@
 
 # Exercise 7-5
 
-prompt = "\nHow old is the moviegoer? (Type 'quit' to exit): "
+# prompt = "\nHow old is the moviegoer? (Type 'quit' to exit): "
 
-response = ""
+# response = ""
 
-while response != "quit":
-    response = input(prompt)
+# while response != "quit":
+#     response = input(prompt)
 
-    if response != "quit":
-        if int(response) < 3:
-            print("Tickets for children under 3yo are free!")
-        elif int(response) <= 12:
-            print("Tickets for children aged between 3 and 12 are $10 each.")
-        else:
-            print("Tickets for adults and children over 12 are $15.")
+#     if response != "quit":
+#         if int(response) < 3:
+#             print("Tickets for children under 3yo are free!")
+#         elif int(response) <= 12:
+#             print("Tickets for children aged between 3 and 12 are $10 each.")
+#         else:
+#             print("Tickets for adults and children over 12 are $15.")
+
+
+# Exercise 7-6
+# variation on Exercise 7-4, using a break statement to exit the loop:
+
+prompt = "\nEnter one pizza topping at a time. Type 'quit' to exit: "
+
+while True:
+    topping = input(prompt)
+
+    if topping == "quit":
+        break
+    else:
+        print(f"Thank you. We have added {topping} to your pizza.")
