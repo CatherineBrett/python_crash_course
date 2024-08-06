4593a8b5bc5f10d62b36bf02f0444e1dc1a9a637
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-06 12:42:59 +0100

    Exercise 7-4 version 2, while loops

--- chapter_07/while_loops.py
+++ chapter_07/while_loops.py
@@ -1,14 +1,27 @@
 # Exercise 7-4
 
-prompt = "\nEnter a pizza topping."
-prompt += "\n(Type 'quit' to finish): "
+# prompt = "\nEnter a pizza topping."
+# prompt += "\n(Type 'quit' to finish): "
 
-active = True
+# active = True
 
-while active:
-    message = input(prompt)
+# while active:
+#     message = input(prompt)
 
-    if message == "quit":
-        active = False
-    else:
-        print(f"Thank you. We have added {message} to your pizza.")
+#     if message == "quit":
+#         active = False
+#     else:
+#         print(f"Thank you. We have added {message} to your pizza.")
+
+
+# Exercise 7-4 version 2
+
+prompt = "\nEnter one pizza topping.(Or type 'quit' to finish): "
+
+response = ""
+
+while response != "quit":
+    response = input(prompt)
+
+    if response != "quit":
+        print(f"Thank you. We have added {response} to your pizza.")
