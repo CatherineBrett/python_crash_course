c7fe945058151c6b51483b170ab548d98965f37f
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-07 16:30:17 +0100

    Exercise 7-10, while loops with lists and dictionaries

--- chapter_07/while_loops_lists_dicts.py
+++ chapter_07/while_loops_lists_dicts.py
@@ -13,27 +13,55 @@
 # for filling in finished_sandwiches:
 #     print(filling)
 
+
 # Exercise 7-9
 
-sandwich_orders = [
-    "pastrami",
-    "tuna mayo",
-    "pastrami",
-    "cheese and tomato",
-    "pastrami",
-    "egg salad",
-]
-finished_sandwiches = []
+# sandwich_orders = [
+#     "pastrami",
+#     "tuna mayo",
+#     "pastrami",
+#     "cheese and tomato",
+#     "pastrami",
+#     "egg salad",
+# ]
+# finished_sandwiches = []
 
-print("We are currently out of pastrami - apologies for any inconvenience.")
+# print("We are currently out of pastrami - apologies for any inconvenience.")
 
-while "pastrami" in sandwich_orders:
-    sandwich_orders.remove("pastrami")
+# while "pastrami" in sandwich_orders:
+#     sandwich_orders.remove("pastrami")
 
-while sandwich_orders:
-    filling = sandwich_orders.pop()
-    finished_sandwiches.append(filling)
+# while sandwich_orders:
+#     filling = sandwich_orders.pop()
+#     finished_sandwiches.append(filling)
 
-print("\nThe sandwiches we have been able to make are:")
-for filling in finished_sandwiches:
-    print(filling)
+# print("\nThe sandwiches we have been able to make are:")
+# for filling in finished_sandwiches:
+#     print(filling)
+
+
+# Exercise 7-10
+
+responses = {}
+
+polling_active = True
+
+while polling_active:
+
+    name = input("\nWhat is your name? ")
+    response = input("\nWhat is your number one dream holiday destination? ")
+
+    responses[name] = response
+
+    repeat = input(
+        "\nWould you like to allow another person to respond to the poll? (y/n): "
+    )
+
+    if repeat == "n":
+        polling_active = False
+
+print("\nThe results of the poll are:")
+for name, response in responses.items():
+    print(f"\t{name}'s dream holiday destination is {response}!")
+
+print(f"\nThe responses dictionary looks like this: {responses}")
