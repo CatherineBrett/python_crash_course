bbeb06b73e49399e8dd526be1c42a931d505cd0d
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-07 16:56:40 +0100

    Exercise 8-2, defining a function

--- chapter_08_functions/defining_a_function.py
+++ chapter_08_functions/defining_a_function.py
@@ -1,9 +1,20 @@
 # Exercise 8-1
 
 
-def display_message():
-    """Display a message about Chapter 8 of Python Crash Course."""
-    print("In Chapter 8, we are learning about functions.")
+# def display_message():
+#     """Display a message about Chapter 8 of Python Crash Course."""
+#     print("In Chapter 8, we are learning about functions.")
 
 
-display_message()
+# display_message()
+
+
+# Exercise 8-2
+
+
+def favourite_book(title):
+    """Display a message naming one of my favourite books."""
+    print(f"One of my favourite books is {title.title()}.")
+
+
+favourite_book("the secret history")
