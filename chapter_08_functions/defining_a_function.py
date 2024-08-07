f60a382f97f290cc035869c0a7a84d9844e1ef46
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-07 16:49:28 +0100

    Exercise 8-1, defining a function

---
+++ chapter_08_functions/defining_a_function.py
@@ -0,0 +1,9 @@
+# Exercise 8-1
+
+
+def display_message():
+    """Display a message about Chapter 8 of Python Crash Course."""
+    print("In Chapter 8, we are learning about functions.")
+
+
+display_message()
