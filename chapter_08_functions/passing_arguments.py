0a9721c046fcbf4f72a2d75ec98f2ea3970ab1d0
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-11 12:47:50 +0100

    Exercise 8-4, passing arguments

--- chapter_08_functions/passing_arguments.py
+++ chapter_08_functions/passing_arguments.py
@@ -1,7 +1,22 @@
 # Exercise 8-3
 
 
-def make_shirt(size, text):
+# def make_shirt(size, text):
+#     """Display info about a shirt's size and its printed text."""
+#     print(
+#         f"\nThe shirt is size {size} and has the following text printed on it: "
+#         f"{text}."
+#     )
+
+
+# make_shirt("small", "This is my favourite t-shirt")
+# make_shirt(text="This is my second-favourite t-shirt", size="medium")
+
+
+# Exercise 8-4
+
+
+def make_shirt(size="large", text="I love Python"):
     """Display info about a shirt's size and its printed text."""
     print(
         f"\nThe shirt is size {size} and has the following text printed on it: "
@@ -9,5 +24,7 @@ def make_shirt(size, text):
     )
 
 
-make_shirt("small", "This is my favourite t-shirt")
-make_shirt(text="This is my second-favourite t-shirt", size="medium")
+make_shirt()
+make_shirt("medium")
+make_shirt("small", "I am learning Python")
+make_shirt(text="I love programming", size="medium")
