47ee76f866c9d74e389ad829120f3ca142c3785f
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-11 12:58:13 +0100

    Exercise 8-5, passing arguments

--- chapter_08_functions/passing_arguments.py
+++ chapter_08_functions/passing_arguments.py
@@ -16,15 +16,29 @@
 # Exercise 8-4
 
 
-def make_shirt(size="large", text="I love Python"):
-    """Display info about a shirt's size and its printed text."""
-    print(
-        f"\nThe shirt is size {size} and has the following text printed on it: "
-        f"{text}."
-    )
+# def make_shirt(size="large", text="I love Python"):
+#     """Display info about a shirt's size and its printed text."""
+#     print(
+#         f"\nThe shirt is size {size} and has the following text printed on it: "
+#         f"{text}."
+#     )
 
 
-make_shirt()
-make_shirt("medium")
-make_shirt("small", "I am learning Python")
-make_shirt(text="I love programming", size="medium")
+# make_shirt()
+# make_shirt("medium")
+# make_shirt("small", "I am learning Python")
+# make_shirt(text="I love programming", size="medium")
+
+
+# Exercise 8-5
+
+
+def describe_city(city, country="england"):
+    """Displays a sentence naming a city and the country in which it is
+    located."""
+    print(f"{city.title()} is in {country.title()}.")
+
+
+describe_city("birmingham")
+describe_city("cardiff", "wales")
+describe_city(country="scotland", city="glasgow")
