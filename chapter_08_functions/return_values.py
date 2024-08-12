4b717e28b2e349ae8965d9543ef21c7d53f4d6c7
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-12 17:26:54 +0100

    Exercise 8-8, return values

--- chapter_08_functions/return_values.py
+++ chapter_08_functions/return_values.py
@@ -19,20 +19,48 @@
 # Exercise 8-7
 
 
-def make_album(artist, album, no_of_songs=None):
+# def make_album(artist, album, no_of_songs=None):
+#     """Return a dictionary containing information about an album."""
+#     album_info = {"artist": artist, "album": album}
+#     if no_of_songs:
+#         album_info["no. of songs"] = no_of_songs
+#     return album_info
+
+
+# first_album = make_album("super furry animals", "guerrilla")
+# second_album = make_album("patti smith", "horses")
+# third_album = make_album("the strokes", "room on fire")
+# fourth_album = make_album("supergrass", "i should coco", 13)
+
+# print(first_album)
+# print(second_album)
+# print(third_album)
+# print(fourth_album)
+
+
+# Exercise 8-8
+
+
+def make_album(artist, album):
     """Return a dictionary containing information about an album."""
     album_info = {"artist": artist, "album": album}
-    if no_of_songs:
-        album_info["no. of songs"] = no_of_songs
     return album_info
 
 
-first_album = make_album("super furry animals", "guerrilla")
-second_album = make_album("patti smith", "horses")
-third_album = make_album("the strokes", "room on fire")
-fourth_album = make_album("supergrass", "i should coco", 13)
+while True:
+    print(
+        "\nPlease give the name of one of your favourite musicians/bands, and "
+        "an album they made."
+    )
+    print("(You can enter 'q' to quit at any time.)")
 
-print(first_album)
-print(second_album)
-print(third_album)
-print(fourth_album)
+    musician_or_band = input("Artist: ")
+    if musician_or_band == "q":
+        break
+
+    record = input("Album name: ")
+    if record == "q":
+        break
+
+    new_album = make_album(musician_or_band, record)
+    print(new_album)
