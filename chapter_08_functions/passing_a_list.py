2be529e762750992980a0d916c7371f748b13d85
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-15 15:35:51 +0100

    Exercise 8-9, passing a list

---
+++ chapter_08_functions/passing_a_list.py
@@ -0,0 +1,12 @@
+# Exercise 8-9
+
+
+def show_messages(list_of_messages):
+    """Prints each message in the list."""
+    for message in list_of_messages:
+        print(message)
+
+
+text_messages = ["many thanks", "let me know", "see you then"]
+
+show_messages(text_messages)
