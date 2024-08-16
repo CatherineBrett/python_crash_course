ce49e93f2325f6355ac96d230f3279c1ea9d0904
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-16 11:52:38 +0100

    Exercise 8-13, passing an arbitrary no. of arguments

--- chapter_08_functions/arbitrary_no_of_arguments.py
+++ chapter_08_functions/arbitrary_no_of_arguments.py
@@ -1,13 +1,34 @@
 # Exercise 8-12
 
 
-def make_sandwich(*fillings):
-    """Print a summary of the sandwich the customer has ordered."""
-    print("\nYour sandwich has the following fillings:")
-    for filling in fillings:
-        print(f"\t- {filling}")
+# def make_sandwich(*fillings):
+#     """Print a summary of the sandwich the customer has ordered."""
+#     print("\nYour sandwich has the following fillings:")
+#     for filling in fillings:
+#         print(f"\t- {filling}")
 
 
-make_sandwich("bacon", "lettuce", "tomato")
-make_sandwich("tuna", "sweetcorn")
-make_sandwich("cheese")
+# make_sandwich("bacon", "lettuce", "tomato")
+# make_sandwich("tuna", "sweetcorn")
+# make_sandwich("cheese")
+
+
+# Exercise 8-13
+
+
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user."""
+    user_info["first_name"] = first
+    user_info["last_name"] = last
+    return user_info
+
+
+user_profile = build_profile(
+    "catherine",
+    "brett",
+    middle_name="jane",
+    location="birmingham",
+    field="software engineering",
+)
+
+print(user_profile)
