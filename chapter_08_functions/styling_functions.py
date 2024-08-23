3f71fbbc22f755ab4a2b79f188be4c62ab203e8c
Catherine Brett <catherinejanebrett@icloud.com>
2024-08-23 08:47:43 +0100

    Exercise 8-17 notes (re styling functions)

---
+++ chapter_08_functions/styling_functions.py
@@ -0,0 +1,25 @@
+# STYLING FUNCTIONS:
+
+# 1. Functions should have descriptive names
+# 2. Using lowercase letters
+# 3. And underscores
+# 4. Module names should use the same conventions
+# 5. Every function needs a comment immediately after the function definition
+# 6. In docstring format
+# 7. Explaining concisely what the function does
+# 8. If you specify a default value for a parameter, no spaces should be used
+# on either side of the equal sign, e.g. parameter_1="default value"
+# 9. The same convention should be used for keyword arguments in function calls
+# 10. Limit lines of code to 79 characters
+# 11. If parameters cause a function's definition to exceed 79 characters,
+# style it like this:
+
+# def function_name(
+#         parameter_0, parameter_1, parameter_2,
+#         parameter_3, parameter_4, parameter_5):
+#     function body...
+
+# 12. If your program/module has more than one function, put two blank lines
+# between them
+# 13. import statements always live at the top of the file
+# 14. (Although comments describing the overall program can go above)
