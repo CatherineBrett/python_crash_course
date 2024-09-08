02fedfc4bad25a615dd16b4ec3265fa5d43e6877
Catherine Brett <catherinejanebrett@icloud.com>
2024-09-08 15:40:09 +0100

    Exercise 9-14, the Python standard library

--- chapter_09_classes/the_python_standard_library.py
+++ chapter_09_classes/the_python_standard_library.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 
 
 class Die:
@@ -17,40 +17,56 @@ class Die:
 
 
 six_sided_die = Die()
-print("Rolling my six-sided die ten times:")
-six_sided_die.roll_die()
-six_sided_die.roll_die()
-six_sided_die.roll_die()
-six_sided_die.roll_die()
-six_sided_die.roll_die()
-six_sided_die.roll_die()
-six_sided_die.roll_die()
-six_sided_die.roll_die()
-six_sided_die.roll_die()
-six_sided_die.roll_die()
+# print("Rolling my six-sided die ten times:")
+# six_sided_die.roll_die()
+# six_sided_die.roll_die()
+# six_sided_die.roll_die()
+# six_sided_die.roll_die()
+# six_sided_die.roll_die()
+# six_sided_die.roll_die()
+# six_sided_die.roll_die()
+# six_sided_die.roll_die()
+# six_sided_die.roll_die()
+# six_sided_die.roll_die()
 
 ten_sided_die = Die(10)
-print("\nRolling my ten-sided die ten times:")
-ten_sided_die.roll_die()
-ten_sided_die.roll_die()
-ten_sided_die.roll_die()
-ten_sided_die.roll_die()
-ten_sided_die.roll_die()
-ten_sided_die.roll_die()
-ten_sided_die.roll_die()
-ten_sided_die.roll_die()
-ten_sided_die.roll_die()
-ten_sided_die.roll_die()
+# print("\nRolling my ten-sided die ten times:")
+# ten_sided_die.roll_die()
+# ten_sided_die.roll_die()
+# ten_sided_die.roll_die()
+# ten_sided_die.roll_die()
+# ten_sided_die.roll_die()
+# ten_sided_die.roll_die()
+# ten_sided_die.roll_die()
+# ten_sided_die.roll_die()
+# ten_sided_die.roll_die()
+# ten_sided_die.roll_die()
 
 twenty_sided_die = Die(20)
-print("\nRolling my twenty-sided die ten times:")
-twenty_sided_die.roll_die()
-twenty_sided_die.roll_die()
-twenty_sided_die.roll_die()
-twenty_sided_die.roll_die()
-twenty_sided_die.roll_die()
-twenty_sided_die.roll_die()
-twenty_sided_die.roll_die()
-twenty_sided_die.roll_die()
-twenty_sided_die.roll_die()
-twenty_sided_die.roll_die()
+# print("\nRolling my twenty-sided die ten times:")
+# twenty_sided_die.roll_die()
+# twenty_sided_die.roll_die()
+# twenty_sided_die.roll_die()
+# twenty_sided_die.roll_die()
+# twenty_sided_die.roll_die()
+# twenty_sided_die.roll_die()
+# twenty_sided_die.roll_die()
+# twenty_sided_die.roll_die()
+# twenty_sided_die.roll_die()
+# twenty_sided_die.roll_die()
+
+
+# Exercise 9-14
+
+lottery = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "a", "b", "c", "d", "e"]
+
+winning_nums_letters = []
+
+while len(winning_nums_letters) < 4:
+    drawn_num_letter = choice(lottery)
+    if drawn_num_letter not in winning_nums_letters:
+        winning_nums_letters.append(drawn_num_letter)
+
+print("Tickets matching these four numbers or letters win a prize:")
+for drawn_num_letter in winning_nums_letters:
+    print(drawn_num_letter)
