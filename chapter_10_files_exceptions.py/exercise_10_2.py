66d00b18f9256e13fb1a9d0c1902bc61b69267cd
Catherine Brett <catherinejanebrett@icloud.com>
2024-09-15 12:43:07 +0100

    Exercise 10-3, files and exceptions

--- chapter_10_files_exceptions.py/exercise_10_2.py
+++ chapter_10_files_exceptions.py/exercise_10_2.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 path = Path("learning_python.txt")
 contents = path.read_text()
-lines = contents.splitlines()
-for line in lines:
+for line in contents.splitlines():
     modified_line = line.replace("Python", "C")
     print(modified_line)
--- chapter_10_files_exceptions.py/learnings.py
+++ chapter_10_files_exceptions.py/learnings.py
@@ -4,6 +4,5 @@ path = Path("learning_python.txt")
 contents = path.read_text(encoding="utf-8")
 print(f"{contents}\n")
 
-lines = contents.splitlines()
-for line in lines:
+for line in contents.splitlines():
     print(line)
