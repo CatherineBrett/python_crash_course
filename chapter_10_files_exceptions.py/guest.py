224873d4fca20fb2b71f6a873a6d82220c732161
Catherine Brett <catherinejanebrett@icloud.com>
2024-09-17 12:18:36 +0100

    Exercise 10-5, writing to a file

--- chapter_10_files_exceptions.py/guest.py
+++ chapter_10_files_exceptions.py/guest.py
@@ -1,5 +1,21 @@
 from pathlib import Path
 
-guest = input("What is your name? ")
-path = Path("guest.txt")
-path.write_text(guest)
+# guest = input("What is your name? ")
+# path = Path("guest.txt")
+# path.write_text(guest)
+
+prompt = "\nWhat is your name?"
+prompt += "\n(Or enter 'quit' to exit): "
+
+guest_list = ""
+
+while True:
+    guest = input(prompt)
+
+    if guest == "quit":
+        break
+    else:
+        guest_list += f"{guest}\n"
+
+path = Path("guest_book.txt")
+path.write_text(guest_list)
