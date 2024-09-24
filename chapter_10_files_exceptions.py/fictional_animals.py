2b7d1d20fe0cb67e48a1c81353350c0ba97f6869
Catherine Brett <catherinejanebrett@icloud.com>
2024-09-24 12:31:23 +0100

    Exercise 10-9, exceptions

--- chapter_10_files_exceptions.py/fictional_animals.py
+++ chapter_10_files_exceptions.py/fictional_animals.py
@@ -1,15 +1,35 @@
 from pathlib import Path
 
 
+# def print_names(path):
+#     """Print the fictional animals named in a given file."""
+#     try:
+#         contents = path.read_text(encoding="utf-8")
+#     except FileNotFoundError:
+#         print(f"Sorry, the file {path} cannot be found.")
+#     else:
+#         # Not bothering with splitlines() here as only printing to screen
+#         # as opposed to wanting to access/modify elements in a list
+#         print(f"{contents}\n")
+
+
+# filenames = ["cats.txt", "dogs.txt"]
+# for filename in filenames:
+#     file = Path(filename)
+#     print_names(file)
+
+
+# Exercise 10-9
+# (Fail silently)
+
+
 def print_names(path):
     """Print the fictional animals named in a given file."""
     try:
         contents = path.read_text(encoding="utf-8")
     except FileNotFoundError:
-        print(f"Sorry, the file {path} cannot be found.")
+        pass
     else:
-        # Not bothering with splitlines() here as only printing to screen
-        # as opposed to wanting to access/modify elements in a list
         print(f"{contents}\n")
 
 
