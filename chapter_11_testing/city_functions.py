8ce25b23d4a0e4255c05eb41da66435149c645bd
Catherine Brett <catherinejanebrett@icloud.com>
2024-10-17 10:41:26 +0100

    Exercise 11-2, testing your code

--- chapter_11_testing/city_functions.py
+++ chapter_11_testing/city_functions.py
@@ -1,4 +1,13 @@
-def get_formatted_place(city, country):
-    """Generate a neatly formatted placename."""
-    placename = f"{city}, {country}"
-    return placename.title()
+# def get_formatted_place(city, country):
+#     """Generate a neatly formatted placename."""
+#     placename = f"{city}, {country}"
+#     return placename.title()
+
+
+def get_formatted_place(city, country, population=""):
+    """Generate neatly formatted location info."""
+    if population:
+        place = f"{city.title()}, {country.title()} - population {population}"
+    else:
+        place = f"{city.title()}, {country.title()}"
+    return place
--- chapter_11_testing/test_cities.py
+++ chapter_11_testing/test_cities.py
@@ -3,5 +3,13 @@ from city_functions import get_formatted_place
 
 def test_city_country():
     """Do placenames like 'Birmingham, England' work?"""
-    formatted_placename = get_formatted_place("birmingham", "england")
-    assert formatted_placename == "Birmingham, England"
+    formatted_place = get_formatted_place("birmingham", "england")
+    assert formatted_place == "Birmingham, England"
+
+
+def test_city_country_population():
+    """Does place info like 'Birmingham, England - population 1144900' work?"""
+    formatted_place = get_formatted_place(
+        "birmingham", "england", population=1144900
+    )
+    assert formatted_place == "Birmingham, England - population 1144900"
